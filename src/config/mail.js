06bbb0d45fade0c4b15d0a22615d84c7900746a0
Yago Bravim <ybribeiro14@gmail.com>
2019-10-18 14:39:37 -0300

    Commit modulo 3

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '27e5d9e029767c',
+    pass: '9fa55d096e8d7a',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
